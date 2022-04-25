c270b08652cf735bbd59cde5f10031ec3c634f85
Nayt <nate.bradshaw5959@gmail.com>
2022-04-25 09:30:39 +0100

    small changes

--- phaser_test.js
+++ phaser_test.js
@@ -13,7 +13,7 @@ class Example extends Phaser.Scene
         this.matter.add.polygon(100, 500, 8, 50, { isStatic: true });
         this.matter.add.rectangle(750, 200, 16, 180, { isStatic: true });
     
-        //  Now a body that shows off internal edges + convex hulls
+        //  Now a body that shows off internal edges + convex hulls this didnt work, dont need it anyway :)
         //const star = '50 0 63 38 100 38 69 59 82 100 50 75 18 100 31 59 0 38 37 38';
     
         //this.matter.add.fromVertices(700, 500, star.split(' ').map(d => parseInt(d)), { restitution: 0.5 }, true);
@@ -25,13 +25,35 @@ class Example extends Phaser.Scene
         
         //  A spring, because length > 0 and stiffness < 0.9
         this.matter.add.spring(body1, body2, 140, 0.001);
+        
+        this.matter.add.spring(body2, body3, 140, 0.001);
     
         //  A joint, because length > 0 and stiffness > 0.1
         this.matter.add.worldConstraint(body3, 140, 1, { pointA: { x: 400, y: 250 }});
+
     
         //  A pin, because length = 0 and stiffness > 0.1
         this.matter.add.worldConstraint(body4, 0, 1, { pointA: { x: 500, y: 50 }});
+
+        /*
+        let collTest = this.matter.add.polygon(200, 200, 5, 40, { restitution: 0.5 });
+
+        let cat1 = this.matter.world.nextCategory();
     
+        collTest.setCollisionCategory(cat1)
+
+        let cat2 = this.matter.world.nextCategory();
+    
+        collTest.setCollidesWith([ cat1, cat2 ]); //if no work move to bottom?
+        // blockA.setCollidesWith(cat1);
+        */
+
+    
+        let tempBody2 = this.matter.add.polygon(200, 200, 5, 40, { restitution: 0.5 });
+        let tempBody1 = this.matter.add.polygon(200, 200, 5, 40, { restitution: 0.5 });
+
+        //tempBody1.setCollisionCategory(cat2)
+        //tempBody2.setCollisionCategory(cat2)
         //  Finally some random dynamic bodies
         for (let i = 0; i < 12; i++)
         {
@@ -43,18 +65,23 @@ class Example extends Phaser.Scene
                 let sides = Phaser.Math.Between(3, 14);
                 let radius = Phaser.Math.Between(8, 50);
     
-                this.matter.add.polygon(x, y, sides, radius, { restitution: 0.5 });
+                tempBody2 = this.matter.add.polygon(x, y, sides, radius, { restitution: 0.5 });
             }
             else
             {
                 let width = Phaser.Math.Between(16, 128);
                 let height = Phaser.Math.Between(8, 64);
     
-                this.matter.add.rectangle(x, y, width, height, { restitution: 0.5 });
+                tempBody2 = this.matter.add.rectangle(x, y, width, height, { restitution: 0.5 });
             }
+            //tempBody2.setCollisionCategory(cat2)
+
+            this.matter.add.spring(tempBody1, tempBody2, 100, 0.9);
+            tempBody1 = tempBody2;
+
         }
     
-        this.matter.add.mouseSpring();
+        this.matter.add.mouseSpring(); //alows mouse interaction
     }
 }
 
