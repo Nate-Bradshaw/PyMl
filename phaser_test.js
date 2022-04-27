cbbbfe08a047ff96b1767619aabf109c2de01dab
Nayt <nate.bradshaw5959@gmail.com>
2022-04-27 11:36:55 +0100

    mini revert 2 steps back to make the test work again

--- phaser_test.js
+++ phaser_test.js
@@ -1,54 +1,50 @@
-
 class Example extends Phaser.Scene
 {
     create ()
     {
-
         this.matter.world.setBounds();
-
         //  First, we'll create a few static bodies
         const body1 = this.matter.add.rectangle(250, 50, 200, 32, { isStatic: true });
     
         this.matter.add.polygon(600, 100, 3, 40, { isStatic: true });
         this.matter.add.polygon(100, 500, 8, 50, { isStatic: true });
         this.matter.add.rectangle(750, 200, 16, 180, { isStatic: true });
-    
+
+        //  Now a body that shows off internal edges + convex hulls
         //  Now a body that shows off internal edges + convex hulls this didnt work, dont need it anyway :)
         //const star = '50 0 63 38 100 38 69 59 82 100 50 75 18 100 31 59 0 38 37 38';
-    
+
         //this.matter.add.fromVertices(700, 500, star.split(' ').map(d => parseInt(d)), { restitution: 0.5 }, true);
     
         //  Some different joint types
         const body2 = this.matter.add.circle(150, 250, 16);
         const body3 = this.matter.add.circle(400, 450, 16);
         const body4 = this.matter.add.circle(500, 50, 16);
-        
+
         //  A spring, because length > 0 and stiffness < 0.9
         this.matter.add.spring(body1, body2, 140, 0.001);
-        
+
         this.matter.add.spring(body2, body3, 140, 0.001);
-    
+
         //  A joint, because length > 0 and stiffness > 0.1
         this.matter.add.worldConstraint(body3, 140, 1, { pointA: { x: 400, y: 250 }});
 
-    
+
         //  A pin, because length = 0 and stiffness > 0.1
         this.matter.add.worldConstraint(body4, 0, 1, { pointA: { x: 500, y: 50 }});
 
         /*
         let collTest = this.matter.add.polygon(200, 200, 5, 40, { restitution: 0.5 });
-
         let cat1 = this.matter.world.nextCategory();
     
         collTest.setCollisionCategory(cat1)
-
         let cat2 = this.matter.world.nextCategory();
     
         collTest.setCollidesWith([ cat1, cat2 ]); //if no work move to bottom?
         // blockA.setCollidesWith(cat1);
         */
 
-    
+
         let tempBody2 = this.matter.add.polygon(200, 200, 5, 40, { restitution: 0.5 });
         let tempBody1 = this.matter.add.polygon(200, 200, 5, 40, { restitution: 0.5 });
 
@@ -64,14 +60,16 @@ class Example extends Phaser.Scene
             {
                 let sides = Phaser.Math.Between(3, 14);
                 let radius = Phaser.Math.Between(8, 50);
-    
+
+                this.matter.add.polygon(x, y, sides, radius, { restitution: 0.5 });
                 tempBody2 = this.matter.add.polygon(x, y, sides, radius, { restitution: 0.5 });
             }
             else
             {
                 let width = Phaser.Math.Between(16, 128);
                 let height = Phaser.Math.Between(8, 64);
-    
+
+                this.matter.add.rectangle(x, y, width, height, { restitution: 0.5 });
                 tempBody2 = this.matter.add.rectangle(x, y, width, height, { restitution: 0.5 });
             }
             //tempBody2.setCollisionCategory(cat2)
@@ -80,7 +78,8 @@ class Example extends Phaser.Scene
             tempBody1 = tempBody2;
 
         }
-    
+
+        this.matter.add.mouseSpring();
         this.matter.add.mouseSpring(); //alows mouse interaction
     }
 }
@@ -96,30 +95,23 @@ const config = {
         matter: {
             enableSleeping: true,
             debug: {
-
                 showAxes: false,
                 showAngleIndicator: true,
                 angleColor: 0xe81153,
-
                 showBroadphase: false,
                 broadphaseColor: 0xffb400,
-
                 showBounds: false,
                 boundsColor: 0xffffff,
-
                 showVelocity: true,
                 velocityColor: 0x00aeef,
-
                 showCollisions: true,
                 collisionColor: 0xf5950c,
     
                 showSeparations: false,
                 separationColor: 0xffa500,
-
                 showBody: true,
                 showStaticBody: true,
                 showInternalEdges: true,
-
                 renderFill: false,
                 renderLine: true,
     
@@ -131,7 +123,6 @@ const config = {
     
                 staticFillColor: 0x0d177b,
                 staticLineColor: 0x1327e4,
-
                 showSleeping: true,
                 staticBodySleepOpacity: 1,
                 sleepFillColor: 0x464646,
@@ -165,5 +156,4 @@ const config = {
     },
     scene: Example
 };
-
 let game = new Phaser.Game(config);
