df1802c8372cc85b21e96c81b1c14e347797dc90
Nayt <nate.bradshaw5959@gmail.com>
2022-06-20 09:14:50 +0100

    I. CAN. DELETE. CONSTRAINTS.
    this.matter.world.removeConstraint(tempConst); = fun :)

--- phaser_collision.js
+++ phaser_collision.js
@@ -40,8 +40,13 @@ function create ()
     blockA = this.matter.add.image(200, 300, 'block').setBounce(1.5).setFriction(0);
     blockA.setScale(0.15);
     blockA.setVelocityX(25);
+
+
     blockB = this.matter.add.image(400, 300, 'block').setStatic(true);
     blockB.setScale(0.15);
+
+    //blockA.setOnCollideWith(blockB.body, onColl(this, "insert other collider"))
+
 
     var blockC = this.matter.add.image(750, 300, 'block').setStatic(true); //right block
     blockC.setScale(0.15);
@@ -63,10 +68,12 @@ function create ()
 
     spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-    this.matter.world.on('collisionstart', function (event) {
+    this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
 
-        event.pairs[0].bodyA.gameObject.setTint(0xff0000);
-        event.pairs[0].bodyB.gameObject.setTint(0x00ff00);
+        bodyA.gameObject.setTint(0xff0000);
+        bodyB.gameObject.setTint(0x00ff00);
+        console.log(bodyA.gameObject3);
+        console.log(bodyB.body);
         //console.log("bonk");
 
     });
@@ -82,12 +89,16 @@ function update ()
         if(colToggle){
             blockB.setCollisionCategory(cat2);
             colToggle = false;
-            console.log("collides");
+            //console.log("collides");
         }
         else if(!colToggle){
             blockB.setCollisionCategory(cat3);
             colToggle = true;
-            console.log("nah");
+            //console.log("nah");
         }
     }
+}
+
+function onColl (body1, body2){
+    console.log(body1, body2);
 }
